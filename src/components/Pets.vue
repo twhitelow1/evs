269f09ad7b75935b1a2ab560c4b6bd57a3287216
Todd Whitelow <todd@whitelow.tech>
2022-06-07 15:14:20 -0600

    create new component for pet services

---
+++ src/components/Pets.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="Pets">
+    <!-- Section Services -->
+      <section id="pets" class="color-section">
+         <div class="container">
+            <div class="col-lg-8 col-lg-offset-2">
+               <!-- Section heading -->
+               <div class="section-heading">
+                  <h2>Pet Sitting Services</h2>
+               </div>
+            </div>
+            <div class="row">
+			<!-- main text -->
+               <div class="col-md-12 col-lg-7 text-center">
+                  <h3 class="text-center">Pet Care Services</h3>
+                  <p>Eagle Valley Sitters is now offering pet sitter serivices.
+                  </p>
+               </div>
+               <!-- /col-sm-12-->
+               <div class="col-md-12 col-lg-5">
+                  <!-- Image -->
+                  <img src="img/petsitting.jpg" alt="petsitting" class="img-responsive center-block">
+               </div>
+               <!-- /col-md-12-->
+            </div>
+         </div>
+         <!-- /container-->
+      </section>
+      <!-- /Section ends -->
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
+
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -30,7 +30,7 @@
             <!-- Text -->
             <div class="ls-l header-text container" data-ls="offsetxin:0;durationin:2000;delayin:1500;easingin:easeOutElastic;rotatexin:-90;transformoriginin:50% top 0;offsetxout:-200;durationout:1000;parallaxlevel:2;">
                <h1>Now Offering Pet Sitting!</h1>
-               <p class="subtitle hidden-xs"> Let us watch over your fur babies.</p>
+               <p class="subtitle hidden-xs"> We provide hourly pet sitting, overnight pet sitting, drop-in care, and dog walks. Let us watch over your fur babies.</p>
                <!-- Button -->
                <div class="page-scroll hidden-xs">
                   <a class="btn" href="https://eaglevalleysitters.enginehire.io/application-form/195/4186">Book A Sitter Today!</a>
