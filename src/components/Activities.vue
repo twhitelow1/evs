cfaa74e5dc0b05f698ad4b915ee37c02084a736d
Todd Whitelow <todd@whitelow.tech>
2022-12-02 20:48:36 -0700

    removed ski chaperone

--- src/components/Activities.vue
+++ src/components/Activities.vue
@@ -13,7 +13,7 @@
                <li><a href="#tab2" data-toggle="tab">Swimming</a></li>
                <li><a href="#tab3" data-toggle="tab">Gondola Rides</a></li>
                <li><a href="#tab4" data-toggle="tab">Playground</a></li>
-               <li><a href="#tab5" data-toggle="tab">Ski Chaperone</a></li>
+               <!-- <li><a href="#tab5" data-toggle="tab">Ski Chaperone</a></li> -->
             </ul>
             <div class="tabbable">
                <div class="tab-content col-md-12 col-centered">
@@ -281,19 +281,19 @@
                   </div>
                   <!-- /#tab4 -->	
                   <!--Tab Content 5 -->
-                  <div class="tab-pane fade" id="tab5">
+                  <!-- <div class="tab-pane fade" id="tab5">
                      <div class="row">
                         <div class="col-md-5 col-lg-5 pull-left">
                            <!-- Activity image-->
-                           <img src="img/skichap.jpg" alt="" class="img-responsive img-circle">						
-                        </div>
-                        <div class="col-md-7 col-lg-7 pull-left">
+                           <!-- <img src="img/skichap.jpg" alt="" class="img-responsive img-circle">						 -->
+                        <!-- </div> -->
+                        <!-- <div class="col-md-7 col-lg-7 pull-left"> -->
                            <!-- Activity text-->
-                           <h3>Ski Chaperone</h3>
+                           <!-- <h3>Ski Chaperone</h3> -->
                            <!-- Accordion -->
-                           <div class="panel-group" id="accordion5">
+                           <!-- <div class="panel-group" id="accordion5"> -->
                               <!-- Question 1 -->
-                              <div class="panel">
+                              <!-- <div class="panel">
                                  <div class="panel-heading">
                                     <h6 class="panel-title">
                                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion5" href="#collapse13">Activity Description</a>
@@ -304,10 +304,10 @@
                                        <p>Book a sitter for a fun ski day at Vail or Beaver Creek! Our sitters are experienced skiers and riders. However, we are not ski coaches. Ski chaperones are only offered for children who already know how to ski. There is an additional $5 per hour fee to book a ski chaperone.</p>
                                     </div>
                                  </div>
-                              </div>
+                              </div> -->
                               <!--/panel -->
                               <!-- Question 2 -->
-                              <div class="panel">
+                              <!-- <div class="panel">
                                  <div class="panel-heading">
                                     <h6 class="panel-title">
                                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion5" href="#collapse14">Age Group</a>
@@ -318,10 +318,10 @@
                                        <p>4-12+</p>
                                     </div>
                                  </div>
-                              </div>
+                              </div> -->
                               <!--/panel -->
                               <!-- Question 3 -->
-                              <div class="panel">
+                              <!-- <div class="panel">
                                  <div class="panel-heading">
                                     <h6 class="panel-title">
                                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion5" href="#collapse16">When?</a>
@@ -332,15 +332,15 @@
                                        <p>Late November - Mid April</p>
                                     </div>
                                  </div>
-                              </div>
+                              </div> -->
                               <!--/panel -->					 
-                           </div>
+                           <!-- </div> -->
                            <!-- /.accordion -->		  
-                        </div>
+                        <!-- </div> -->
                         <!-- /.col-md-7 -->
-                     </div>
+                     <!-- </div> -->
                      <!-- /.row -->
-                  </div>
+                  <!-- </div> --> -->
                   <!-- /#tab5 -->					
                </div>
                <!--tab-content-->  
