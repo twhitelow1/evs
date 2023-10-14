9beb8bc680ae65665a75aca9d95746b389c3b0cb
Todd Whitelow <duice352@gmail.com>
2023-10-13 22:38:05 -0600

    add long term nanny page

--- src/components/LongTermNanny.vue
+++ src/components/LongTermNanny.vue
@@ -11,20 +11,25 @@
                  </div>
                  <!-- /col-md-12-->
               <!-- main text -->
-                 <div class="col-md-12 col-lg-7 text-center">
+                 <div class="col-md-12 p-5 col-lg-7 nanny-placement-content">
                     <h3 class="text-center">Long Term Nanny Placement</h3>
                     <h5 class="text-left">We refer some of the best nannies and sitters in Colorado</h5>
                     <p>Each Eagle Valley Sitters candidate must pass our multi-step screening process which includes:</p>
-                    <ul>
-                        <li>Resume Check that verifies previous childcare experience of at least 2 years</li>
-                        <li>Series of in-depth interviews </li>
-                        <li>Social media screen</li>
-                        <li>Background check and Reference check *we use an FCRA compliant screening and require a minimum of 2 professional references </li>
-                        <li>Pediatric CPR certification verification</li>
-                    </ul>
-                    <p>Client satisfaction is our top priority. After a thorough consultation with one of our team members, we start a uniquely tailored search based on your family’s needs. We broadcast to a wide network both within our pool of sitters, and through outer-agency channels. Eagle Valley Sitters offers a 1 month free replacement period to ensure a perfect fit. We also offer backup care options in the event your nanny is sick or unable to work*
-                    </p>
-                    <p>Subject to availability</p>
+                    <div class="p-50">
+                        <ul class="text-left">
+                           <li>Resume Check that verifies previous childcare experience of at least 2 years</li>
+                           <li>Series of in-depth interviews </li>
+                           <li>Social media screen</li>
+                           <li>Background check and Reference check *we use an FCRA compliant screening and require a minimum of 2 professional references </li>
+                           <li>Pediatric CPR certification verification</li>
+                        </ul>
+                     </div>
+                    <h5 class="text-left">Client satisfaction is our top priority. </h5>
+                    <p>After a thorough consultation with one of our team members, we start a uniquely tailored search based on your family’s needs. </p>
+                     <p>We broadcast to a wide network both within our pool of sitters, and through outer-agency channels. </p>
+                     <p>Eagle Valley Sitters offers a 1 month free replacement period to ensure a perfect fit. </p>
+                     <p> We also offer backup care options in the event your nanny is sick or unable to work*</p>
+                    <p class="font-italic font-weight-bold">*Subject to availability</p>
                     
                  </div>
               </div>
@@ -36,6 +41,15 @@
   </template>
   
   <style>
+  .nanny-placement-content {
+    padding-top: 1em !important;
+    padding: 4em;
+  }
+  .nanny-placement-content h5 {
+    font-size: 1.5em;
+    font-weight: 600;
+    margin-bottom: 1em;
+  }
   </style>
   
   <script>
